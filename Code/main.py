a36b2cb57224e0a6592da016ce274d0493d0d325
TropfenderWasserhahn <jensbo@ethz.ch>
2025-02-25 10:58:26 +0100

    Structure

--- Code/main.py
+++ Code/main.py
@@ -4,3 +4,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 print("Module erfolgreich geladen!")
+
+# Load data
+measurement_data = pd.read_csv("Data/Measurement.csv")
+screening_data = pd.read_csv("Data/Screening.csv")
+piads_data = pd.read_csv("Data/PIADS.csv")
+interview_data = pd.read_csv("Data/Interview.csv")
+
+# Data exploration
+def data_exploration(data):
+    print(data.head(10))
+    print(data.shape)
+    print(data.columns)
+    print(data.info())
+    print(data.describe())
+    print(data.isna().sum())
+    print('############################################################')
+
+data_exploration(measurement_data)
+data_exploration(screening_data)
+data_exploration(piads_data)
+data_exploration(interview_data)
