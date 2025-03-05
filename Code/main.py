0c802f38edc6629aa96ef8cd434605e33d0485a6
TropfenderWasserhahn <jensbo@ethz.ch>
2025-03-05 15:12:58 +0100

    Merging structure

--- Code/main.py
+++ Code/main.py
@@ -7,12 +7,13 @@ import os
 print("Module erfolgreich geladen!")
 
 # Load data
-measurement_data = pd.read_csv("Data/Measurement.csv")
+measurement_data_M1 = pd.read_csv("Data/M1.csv")
+measurement_data_M2 = pd.read_csv("Data/M2.csv")
 screening_data = pd.read_csv("Data/Screening.csv")
 piads_data = pd.read_csv("Data/PIADS.csv")
 interview_data = pd.read_csv("Data/Interview.csv")
 
-# Data exploration
+# Data exploration (just for first visualisation, will be deleted later)
 def data_exploration(data):
     print(data.head(10))
     print(data.shape)
@@ -22,7 +23,30 @@ def data_exploration(data):
     print(data.isna().sum())
     print('############################################################')
 
-data_exploration(measurement_data)
-data_exploration(screening_data)
-data_exploration(piads_data)
-data_exploration(interview_data)
+"""
+Merge all data to Screening data so the merged DataFrame will have to following order:
+Screening ¦ Measurement M1 ¦ Interview M1 ¦ PIADS M1 ¦ Measurement M2 ¦ Interview M2 ¦ PIADS M2
+The 'Patient ID' is the unique identifier for the data
+The randomization is set in the Screening dataframe
+The prefixes 'M1_' and 'M2_' are the identifiers for the measurementscores and -times 
+"""
+
+# Set Screening data as base
+merged_data = screening_data
+# List of dataframes that wil be merged in correct order
+dfs = [measurement_data_M1, measurement_data_M2, interview_data, piads_data]
+# Function for selective merging according to prefix M1_ or M2_
+def merge_selected_columns(df_base, dfs, prefix, id_col):
+    for df in dfs:
+        selected_cols = [id_col] + [col for col in df.columns if col.startswith(prefix)]
+        if len(selected_cols) > 1:  # Nur mergen, wenn Spalten mit dem Präfix existieren
+            df_base = pd.merge(df_base, df[selected_cols], on=id_col, how="left")
+    return df_base
+
+# Merge all columns from the dataframes that start with 'M1_'
+merged_data = merge_selected_columns(merged_data, dfs, "M1_", "Patient ID")
+
+# Merge all columns from the dataframes that start with 'M2_'
+merged_data = merge_selected_columns(merged_data, dfs, "M2_", "Patient ID")
+
+data_exploration(merged_data)
